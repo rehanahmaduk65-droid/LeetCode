cc23d7c7def20f0f9ad3a311becf265a5d8d6cfe
Rehan Ahmad <rehanahmaduk65@gmail.com>
2026-08-19 19:25:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (20.11%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long low = 1 , high = num;
+        while(low<=high){
+            long mid = low + (high-low)/2;
+            long square = mid*mid;
+            if(square==num) return true;
+            if(square < num ) low = mid +1;
+            else high = mid-1;
+        }
+        return false;
+    }
+}
