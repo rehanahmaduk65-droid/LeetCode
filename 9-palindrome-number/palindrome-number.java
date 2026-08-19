889d1c827b71bd864f1272f9c2df148df7c249af
Rehan Ahmad <rehanahmaduk65@gmail.com>
2026-08-19 19:29:23 +0530

    Time: 5 ms (81.49%) | Memory: 46 MB (33.60%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int n = x;
+        int reverse = 0;
+        while(n>0){
+            int d = n%10;
+            reverse =  reverse*10 + d;
+            n = n/10;
+        }
+        if (reverse==x){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
