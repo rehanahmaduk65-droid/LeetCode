1ddd161e4c92ebca41a9f864789179838c0f8e9a
Rehan Ahmad <rehanahmaduk65@gmail.com>
2026-08-19 19:39:11 +0530

    Time: 5 ms (82.91%) | Memory: 77.5 MB (29.71%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int start = 0;
+        int end = height.length-1;
+        int maxcap = 0;
+        while(start<end){
+            int h = Math.min(height[start],height[end]);
+            int w = end-start;
+            int currcap= h*w;
+
+            maxcap = Math.max(currcap,maxcap);
+            if(height[start]<height[end]){
+                start++;
+            }else{
+                end--;
+            }
+        }
+        return maxcap;
+    }
+}
